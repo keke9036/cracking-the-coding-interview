27d07661f85b8a1b88edd483410af5cca0480735
keke9036 <keke9036@qq.com>
2016-12-26 20:12:19 +0800

    Add project structure

---
+++ project/src/main/java/com/github/taourist/section_1/FindRepeatedChar.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016 TP-LINK Technologies Co., Ltd. All rights reserved.
+ *
+ * Project: cracking-the-code-interview
+ *
+ */
+
+package com.github.taourist.section_1;
+
+import com.github.taourist.util.StringUtils;
+
+/**
+ * @author Tyrian
+ * @content 1.1 实现一个算法，，确定一个字符串的所有字符是否全都不同。假使不适用额外的数据结构，又该如何处理
+ */
+public class FindRepeatedChar {
+    
+    public static boolean containsRepeatedChar(String s) {
+        if (StringUtils.isEmpty(s)) {
+            throw new IllegalArgumentException("Empty string");
+        }
+        
+        
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ project/src/main/java/com/github/taourist/util/StringUtils.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2016 TP-LINK Technologies Co., Ltd. All rights reserved.
+ *
+ * Project: cracking-the-code-interview
+ *
+ */
+
+package com.github.taourist.util;
+
+/**
+ * @author Tyrian
+ * @version 2016/12/26
+ */
+
+public class StringUtils {
+    
+    public static boolean isEmpty(String s) {
+        return s == null || s.length() <= 0;
+    }
+}
